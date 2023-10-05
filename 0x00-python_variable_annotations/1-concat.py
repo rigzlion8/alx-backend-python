552110cce14808917e5e48610167c82c226aa38b
Reagan Musa <reagan.musav@gmail.com>
2023-10-05 17:55:25 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: Task 1: Basic annotations - concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''This function concatenates two strings.
+    '''
+    return str1 + str2
